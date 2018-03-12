777aeff4f20d3c580288907e9b21de2783f9f346
Kyle Bihr <kyle.bihr@gmail.com>
2018-03-11 21:59:47 -0500

    Add inital setup script

---
+++ setup.py
@@ -0,0 +1,5 @@
+# TODO
+# Create a script that sets up the exercises for the class
+# - Create example exercise directory structure under /home/pi
+#   - Structure TBD
+# - Copy all the examples and exercises into directory structure
