9ff7b208081608257aed51936a2faed11c7fc3f6
KyleBihr <34008653+KyleBihr@users.noreply.github.com>
2018-06-11 20:53:17 -0500

    Add explicit examples to exercise 3

--- exercises/3_data_output/data_output.py
+++ exercises/3_data_output/data_output.py
@@ -15,32 +15,38 @@ display_number = 1
 # Create a Sense HAT interface
 sense = SenseHat()
 
-# Set up some Sense HAT display colors. Colors are represented as
-# lists containg R-G-B (red, green, blue) values between 0 and 255.
-red = [255,0,0]
-green = [0,255,0]
-blue = [0,0,255]
+# Set up the Sense HAT display color. Colors are represented as a
+# list containg R-G-B (red, green, blue) values between 0 and 255.
+# Try changing the values to get a different color
+color = [255,0,0]
 
 # Display the text string on the LED display using the
 # Sense HAT's show_meesage function.
 # text_colour isn't a typo, the Raspberry Pi originated from the United Kingdom,
 # and must use their spelling for color.
-# Example: sense.show_message(message, text_colour=red)
 # TODO Students should add their own show_message here
-
+'''
+Example: sense.show_message(message, text_colour=color)
+'''
 
 # Wait the number of seconds passed to sleep
 sleep(2)
 
 # Display a numeric value on the LED display using the
 # Sense HAT's show_letter function. The number must be turned into
-# a string like str(1) Example:
-# sense.show_letter(str(display_number), text_colour=blue)
+# a string like str(1) 
 # TODO Students should add their own show_letter here
-
+'''
+Example:
+sense.show_letter(str(display_number), text_colour=color)
+'''
 
 # Wait the number of seconds passed to sleep
 # TODO Students should add another sleep here
+'''
+Example:
+sleep(1)
+'''
 
 # Clear the LED display
 sense.clear()
